361e17221f973aac6a7e1ca7b0c9402d3ba60374
Kev Nangcas <knangcas@asu.edu>
2024-03-05 16:49:52 -0600

    changed to switch

--- src/main/java/org/example/Insert.java
+++ src/main/java/org/example/Insert.java
@@ -42,18 +42,34 @@ public class Insert {
         }
         yearPurchased = s.nextInt();
 
-        System.out.println("What is the style? Enter a number.\n"
-                + "1. Casual.\n"
-                + "2. Basketball.\n"
-                + "3. Running.\n"
-                + "4. Other.");
+        style = -1;
 
-        while(!s.hasNextInt()) {
-            System.out.println("Please type a number.");
-            s.next();
+        while (style <0 || style >3) {
+
+
+            System.out.println("What is the style? Enter a number.\n"
+                    + "0. Casual.\n"
+                    + "1. Basketball.\n"
+                    + "2. Running.\n"
+                    + "3. Other.");
+
+            while (!s.hasNextInt()) {
+                System.out.println("Please type a number.");
+                s.next();
+            }
+
+            style = s.nextInt();
+
+            switch (style) {
+                case 0: break;
+                case 1: break;
+                case 2: break;
+                case 3: break;
+                default:
+                    System.out.println("Invalid entry. Try again."); break;
+            }
+
         }
-
-        style = s.nextInt();
 
         System.out.println("What is the shoe size (US)?");
 
@@ -68,53 +84,75 @@ public class Insert {
 
         colorway = s.next();
 
-        System.out.println("What is the primary color?");
-        System.out.println(
-                "0. Black\n"
-                + "1. White\n"
-                + "2. Grey\n"
-                + "3. Red\n"
-                + "4. Green\n"
-                + "5. Blue\n"
-                + "6. Yellow\n"
-                + "7. Brown\n"
-                + "8. Orange\n"
-                + "9. Purple\n"
-                + "10. Gold\n"
-                + "11. Silver\n"
-                + "12. Multi-Color"
-        );
+        primaryColor = -1;
 
-        while(!s.hasNextInt()) {
-            System.out.println("Please type a number.");
-            s.next();
+        while(primaryColor <0 || primaryColor > 12) {
+
+            System.out.println("What is the primary color?");
+            System.out.println(
+                    "0. Black\n"
+                            + "1. White\n"
+                            + "2. Grey\n"
+                            + "3. Red\n"
+                            + "4. Green\n"
+                            + "5. Blue\n"
+                            + "6. Yellow\n"
+                            + "7. Brown\n"
+                            + "8. Orange\n"
+                            + "9. Purple\n"
+                            + "10. Gold\n"
+                            + "11. Silver\n"
+                            + "12. Multi-Color"
+            );
+
+            while (!s.hasNextInt()) {
+                System.out.println("Please type a number.");
+                s.next();
+            }
+
+            primaryColor = s.nextInt();
+            switch (primaryColor) {
+                case 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 -> {
+                }
+                default -> System.out.println("Invalid entry.");
+            }
+
         }
 
-        primaryColor = s.nextInt();
+        secondaryColor = -1;
 
-        System.out.println("What is the secondary color?");
-        System.out.println(
-                "0. Black\n"
-                        + "1. White\n"
-                        + "2. Grey\n"
-                        + "3. Red\n"
-                        + "4. Green\n"
-                        + "5. Blue\n"
-                        + "6. Yellow\n"
-                        + "7. Brown\n"
-                        + "8. Orange\n"
-                        + "9. Purple\n"
-                        + "10. Gold\n"
-                        + "11. Silver\n"
-                        + "12. Multi-Color"
-        );
+        while(primaryColor <0 || primaryColor > 12) {
 
-        while(!s.hasNextInt()) {
-            System.out.println("Please type a number.");
-            s.next();
+            System.out.println("What is the secondary color?");
+            System.out.println(
+                    "0. Black\n"
+                            + "1. White\n"
+                            + "2. Grey\n"
+                            + "3. Red\n"
+                            + "4. Green\n"
+                            + "5. Blue\n"
+                            + "6. Yellow\n"
+                            + "7. Brown\n"
+                            + "8. Orange\n"
+                            + "9. Purple\n"
+                            + "10. Gold\n"
+                            + "11. Silver\n"
+                            + "12. Multi-Color"
+            );
+
+            while (!s.hasNextInt()) {
+                System.out.println("Please type a number.");
+                s.next();
+            }
+
+            secondaryColor = s.nextInt();
+            switch (secondaryColor) {
+                case 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 -> {
+                }
+                default -> System.out.println("Invalid entry.");
+            }
+
         }
-
-        secondaryColor = s.nextInt();
 
 
 
